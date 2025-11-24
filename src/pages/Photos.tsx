f20f7be1eb21a1f2610ca5bc1a1f91bacb9f2cb9
get2bash <get2bash@gmail.com>
2025-11-24 15:33:25 +0100

    added skeleton loading to the photos page

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react'
 import './Photos.css'
 
 const photos = [
@@ -13,14 +14,27 @@ const photos = [
   '/IMG_1165.jpeg',
 ]
 
+function PhotoItem({ src, index }: { src: string, index: number }) {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  return (
+    <div className={`photo-item ${!isLoaded ? 'loading' : ''}`}>
+      <img 
+        src={src} 
+        alt={`Photo ${index + 1}`} 
+        loading="lazy" 
+        onLoad={() => setIsLoaded(true)}
+      />
+    </div>
+  )
+}
+
 export default function Photos() {
   return (
     <div className="photos-page">
       <div className="photos-grid">
         {photos.map((src, index) => (
-          <div key={index} className="photo-item">
-            <img src={src} alt={`Photo ${index + 1}`} loading="lazy" />
-          </div>
+          <PhotoItem key={index} src={src} index={index} />
         ))}
       </div>
     </div>
